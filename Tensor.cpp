656f1c465be7ffba56805f71117e6f50a6411f9e
fvale <ferrel.infante@utec.edu.pe>
2026-04-01 14:31:39 -0500

    creacion de Tensor.h

--- Tensor.cpp
+++ Tensor.cpp
@@ -1,3 +1,24 @@
 #include "Tensor.h"
-
 using namespace std;
+
+size_t Tensor::compute_total_size(const vector<size_t>& shape) {
+    if (shape.empty()) {
+        cout << "El shape no puede estar vacío." << endl;
+    }
+    if (shape.size() > 3) {
+        cout <<"El tensor no puede tener más de 3 dimensiones." << endl;
+    }
+
+    size_t total = 1;
+
+    for (size_t dim : shape) {
+        if (dim == 0) {
+            cout << "Las dimensiones deben ser mayores que 0." << endl;
+        }
+        total *= dim;
+    }
+    return total;
+}
+
+Tensor::Tensor() : shape_{0}, total_size_(0), data_(nullptr) {}
+
