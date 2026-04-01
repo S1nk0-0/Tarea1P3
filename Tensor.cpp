15e1127accc749866f245f49bfa339f3c5bd18f1
fvale <ferrel.infante@utec.edu.pe>
2026-04-01 11:51:35 -0500

    creacion de Tensor.h

---
+++ Tensor.cpp
@@ -0,0 +1,3 @@
+#include "Tensor.h"
+
+using namespace std;
---
+++ Tensor.h
@@ -0,0 +1,36 @@
+#ifndef TAREA1P3_TENSOR_H
+#define TAREA1P3_TENSOR_H
+
+#include "string"
+#include "iostream"
+#include "vector"
+using namespace std;
+
+class Tensor {
+    vector<size_t> shape;
+    vector<double> values;
+    int totaln;
+    double* data;
+
+
+public:
+    Tensor();
+
+    Tensor(const vector<size_t>& shape, const vector<double>& values);
+
+    Tensor(const Tensor& other);                  // copia
+    Tensor& operator=(const Tensor& other);       // asignación copia
+    Tensor(Tensor&& other);              // movimiento
+    Tensor& operator=(Tensor&& other);   // asignación movimiento
+    ~Tensor();                                    // destructor
+
+    // Getters
+    const std::vector<size_t>& shape() const;
+    size_t size() const;
+    const double* data() const;
+    double* data();
+
+    void print_info() const;
+};
+
+#endif
