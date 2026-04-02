a54e1b80b9a4958beaafc7802a692a75276c8b0e
Denilson <denilson.cuadros@utec.edu.pe>
2026-04-02 18:23:46 -0500

    Se crea el .h Para el polimorfismo dinamico

--- TensorTranform.h
+++ TensorTranform.h
@@ -1,16 +1,20 @@
-//
-// Created by Usuario on 2/04/2026.
-//
+#ifndef TENSORTRANSFORM_H
+#define TENSORTRANSFORM_H
+#include "Tensor.h"
+//Clase abstracta
 
-#ifndef HACKATON1_TENSORTRANFORM_H
-#define HACKATON1_TENSORTRANFORM_H
-
-
-
-class TensorTranform {
-
+class TensorTransform {
+public :// Permite aplicar una transformación al tensor actual.
+// Recibe un objeto de tipo TensorTransform (o una clase derivada)
+// y ejecuta su método apply usando polimorfismo dinámico.
+// *this representa el tensor actual que será transformado.
+// Retorna un nuevo tensor con la transformación aplicada.
+virtual Tensor apply( const Tensor & t ) const = 0;
+    virtual ~ TensorTransform () = default ;
 };
+//Plantilla para crear transformaciones
+//Tensor actual - enviar a transformador - recibir nuevo tensor
 
 
 
-#endif //HACKATON1_TENSORTRANFORM_H
+#endif
